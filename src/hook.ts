f5be5a4b96538699f249ebbdd55de1e674d07474
Allister Driscoll <allisterdrisc@gmail.com>
2025-07-16 13:52:21 -0400

    Remove hookContext from spanData object returned by after method
    Co-authored-by: alexwjohnson964 <162201012+alexwjohnson964@users.noreply.github.com>
    Co-authored-by: ScottVenkataraman <109530825+ScottVenkataraman@users.noreply.github.com>
    Co-authored-by: PatrickMayoMoran <32110056+PatrickMayoMoran@users.noreply.github.com>

--- src/hook.ts
+++ src/hook.ts
@@ -18,7 +18,7 @@ export class FlagEnrichmentHook implements Hook {
   async after(hookContext: HookContext, evaluationDetails: EvaluationDetails<any>) {
     const activeContext = context.active();
     const span = trace.getSpan(activeContext);
-    const spanContext = span.spanContext()
+    const spanContext = span.spanContext();
     const spanData = {
       traceId: spanContext.traceId,
       spanId: spanContext.spanId,
@@ -29,7 +29,6 @@ export class FlagEnrichmentHook implements Hook {
       attributes: span.attributes,
       status: span.status,
       events: span.events,
-      hookContext,
       evaluationDetails
     };
     await axios.post('http://localhost:3000/api/flags/telemetry', {telemetry: spanData}, axiosConfig);
