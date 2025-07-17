006fb8fd9deb40301b606c1819f351ad15ad35db
Alex Johnson <alexwjohnson964@gmail.com>
2025-07-17 11:09:46 -0400

    add otlp trace exporter

--- src/telemetry.ts
+++ src/telemetry.ts
@@ -1,19 +1,50 @@
 import { NodeSDK } from '@opentelemetry/sdk-node';
 // import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-base';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
-import { context, trace } from '@opentelemetry/api';
+import { metrics, context, trace } from '@opentelemetry/api';
+import { MeterProvider, ConsoleMetricExporter, PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
 import { AsyncHooksContextManager } from '@opentelemetry/context-async-hooks';
 import { Request, Response, NextFunction } from 'express';
+import {OTLPTraceExporter} from '@opentelemetry/exporter-trace-otlp-proto'
+import {
+  defaultResource,
+  resourceFromAttributes,
+} from '@opentelemetry/resources';
+// import {
+//   ATTR_SERVICE_NAME,
+//   ATTR_SERVICE_VERSION,
+// } from '@opentelemetry/semantic-conventions';
+// const resource = defaultResource().merge(
+//   resourceFromAttributes({
+//     [ATTR_SERVICE_NAME]: 'dice-server',
+//     [ATTR_SERVICE_VERSION]: '0.1.0',
+//   }),
+// );
 
+const metricReader = new PeriodicExportingMetricReader({
+  exporter: new ConsoleMetricExporter(),
+  exportIntervalMillis: 5000,
+});
+const myMeter = metrics.getMeter(
+  'instrumentation-scope-name',
+  'instrumentation-scope-version',
+);
 export const sdk = new NodeSDK({
   contextManager: new AsyncHooksContextManager().enable(),
   // traceExporter: new ConsoleSpanExporter(),
+  traceExporter: new OTLPTraceExporter({
+    url: "http://localhost:4318/v1/traces"
+  }),
+  metricReader: metricReader,
+  
   instrumentations: [getNodeAutoInstrumentations()],
 });
 
 // eslint-disable-next-line max-len
 export const telemetryMiddleware = (req: Request, res: Response, next: NextFunction) => {
   const tracer = trace.getTracer('my-app');
+
+  //TODO: tracer.startActiveSpan? See OTEL instrumentation docs
   const span = tracer.startSpan('http_request', {
     attributes: {
       'http.method': req.method,
