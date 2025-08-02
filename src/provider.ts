b0f7101d0a172782fda96846affa5d093e266282
Scott Venkataraman <scottvenkataraman22@gmail.com>
2025-08-02 11:20:37 -0400

    Add caching to provider.ts file
    Co-authored-by: alexwjohnson964 <162201012+alexwjohnson964@users.noreply.github.com>
    Co-authored-by: allisterdrisc <115250579+allisterdrisc@users.noreply.github.com>
    Co-authored-by: PatrickMayoMoran <32110056+PatrickMayoMoran@users.noreply.github.com>

--- src/provider.ts
+++ src/provider.ts
@@ -14,22 +14,60 @@ const axiosConfig = {
     'Content-Type': 'application/json',
   }
 };
+
+const flagEvaluationCache = new WeakMap();
+
+const getFlagEvaluationConfig = async (evaluationContext: EvaluationContext) => {
+  //TODO: For now, fetch evaluation for all flags for the given context 
+  const response = await axios.post(`${config.apiBaseUrl}/api/evaluate/config`, { context: evaluationContext }, axiosConfig);
+  // 3001
+  // Set the flag evaluation result for each flag
+    //  console.log('Response Data:', response.data)
+  Object.entries(response.data).forEach((result: Record<string, any>) => {
+
+    console.log('result:', result)
+    let configCache = new Map();
+    configCache.set(result[0], result[1]);
+    flagEvaluationCache.set(evaluationContext, configCache)
+  });
+}
+
 const getFlagEvaluation = async (flagKey: string, defaultValue: DefaultValue, context: EvaluationContext)=> {
   const flagDetails = {
     context,
     flagKey
   }
   try {
+    console.log("Made it to getFlagEvaluation try block");
+    if (flagEvaluationCache.has(context)) {
+      console.log("Made it to first if block");
+      let flagValues = flagEvaluationCache.get(context);
+      let value = flagValues.get(flagKey);
+      let evaluation = { 
+        value: value,
+        reason: 'CACHED'
+      }
+      console.log(evaluation);
+      return evaluation;  
+    }
+    
     const response = await axios.post(`${config.apiBaseUrl}/api/evaluate`, flagDetails, axiosConfig);
     if (response === null) {
+      console.log("Response === null, made it to if block");
       return {
         value: defaultValue,
         reason: 'STATIC'
       }
-    } else {
-      console.log('response:', response.data)
-      return response.data;
-    }
+    } 
+      console.log("Adding to cache");
+      const result = response.data;
+      console.log(result);
+      let configCache = new Map();
+      configCache.set(flagKey, result.value);
+      console.log("config cache:", configCache);
+      flagEvaluationCache.set(context, configCache);
+      console.log("result:", result);
+      return result;
   } catch (err) {
     console.error(err)
       return {
